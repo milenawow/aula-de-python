37637fddc44d47285bf6d0edafe8ea278f933e8b
mile <2024328348@ifam.edu.br>
2025-03-17 19:50:32 -0400

    Add files via upload

---
+++ sorteio numero.py
@@ -0,0 +1,10 @@
+import random
+def sorteio_numero():
+    numeros = [
+       "17","20","32"
+       "10","4","9"
+    ]
+    escolher = random.choice(numeros)
+    print(f"o numero escolhido foi: {escolher}")
+
+sorteio_numero()
